3747ec57a530eeda570e74c4e413b113c8f9d0f1
shubhchauhan214 <shubhchauhan616@gmail.com>
2026-03-29 20:01:29 -0700

    Updated Middleware in CORS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from routes import chat
 
@@ -8,9 +9,16 @@ load_dotenv()
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Chat API with Anthropic", description="Simple chatbot using FastAPI + Anthropic Clause",
+app = FastAPI(title="Chat API with GROQ", description="Simple chatbot using FastAPI + GROQ",
               version="1.0.0")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(chat.router)
 
